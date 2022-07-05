72c7f5c5a45a4fd5e97ffb4353685e5e7fe4de0b
jayduco <jayduco2@gmail.com>
2022-07-05 12:23:41 +0800

    Add files via upload

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,22 @@
-let x = 1;
-let y = 1;
-let z = y;
+let f1 = 1;
+let f2 = 1;
+let z = f2;
 let m = "";
+let evenTotal = null;
 
-function fibonacci(n){
+function fibFirstOf(n){
     for (i=1; i<=n; i++){
         m = m + z.toString() + " ";
-        z = x + y;
-        x=y;
-        y=z;
+        z = f1 + f2;
+        f1=f2;
+        f2=z;
+        
+        if(f1%2 == 0){
+            evenTotal = evenTotal + f1;
+        }
     }
-    console.log(m);
+    console.log('Finacci Sequence: '+m);
+    console.log('Total of Even Numbers: ' +evenTotal);
 }
 
-fibonacci (10);
+fibFirstOf (10);
